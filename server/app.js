3217a667c50c741b15c7fdd318befdb6dc508db1
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-03-28 11:55:59 +0200

    fetching urls

--- server/app.js
+++ server/app.js
@@ -1,11 +1,16 @@
 const express = require("express");
-
+const cors = require("cors")
 const app = express()
-const PORT = 5000;
+const PORT = 3000;
+const {productRoutes} = require("./controllers/products.js")
+
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("hi from server");
 });
+
+app.use("/products", productRoutes)
 
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
